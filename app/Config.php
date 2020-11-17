c38bda62b749f891863507df4b818f972907eb8a
Eskith <74556018+Eskith@users.noreply.github.com>
2020-11-17 20:13:19 +0100

    Add files via upload

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos22020";
+     static public $mvc_bd_usuario  = "mygonzaloi";
+     static public $mvc_bd_clave    = "7JIPeld3";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
